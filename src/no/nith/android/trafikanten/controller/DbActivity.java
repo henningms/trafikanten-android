8e96fb7f0e9eee27536ffc0e24bcce2dfaa75a87
Arild Wanvik Tvergrov <arild.wanvik@gmail.com>
2011-10-31 21:33:22 +0100

    Resolve manifest conflict

---
+++ src/no/nith/android/trafikanten/controller/DbActivity.java
@@ -0,0 +1,18 @@
+package no.nith.android.trafikanten.controller;
+
+public class DbActivity extends BaseActivity {
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void controller() {
+	}
+
+	@Override
+	public void view() {
+		setLayout("main");
+	}
+
+}
---
+++ src/no/nith/android/trafikanten/db/DBManager.java
@@ -0,0 +1,11 @@
+package no.nith.android.trafikanten.db;
+
+public class DBManager {
+	
+	private void open() {
+	}
+	
+	private void close() {
+	}
+	
+}
