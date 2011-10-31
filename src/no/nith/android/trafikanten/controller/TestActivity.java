ed39c5531ee0b8f18f65359599a6ca63c44d730f
Arild Wanvik Tvergrov <arild.wanvik@gmail.com>
2011-10-31 17:15:01 +0100

    Ignore gen folder on git

---
+++ src/no/nith/android/trafikanten/controller/TestActivity.java
@@ -0,0 +1,20 @@
+package no.nith.android.trafikanten.controller;
+
+public class TestActivity extends BaseActivity {
+
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void controller() {
+		
+	}
+
+	@Override
+	public void view() {
+		
+	}
+
+}
