8de7f20c46482a944218c319c8191712120c3ea8
Arild Wanvik Tvergrov <arild.wanvik@gmail.com>
2011-10-31 17:27:24 +0100

    Ignore bin folder

--- gen/no/nith/android/trafikanten/R.java
+++ gen/no/nith/android/trafikanten/R.java
@@ -16,9 +16,14 @@ public final class R {
     public static final class id {
         public static final int myButton=0x7f050001;
         public static final int test=0x7f050000;
+        public static final int testButton=0x7f050003;
+        public static final int testEditText=0x7f050004;
+        public static final int testListView=0x7f050005;
+        public static final int testTextView=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int test=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
